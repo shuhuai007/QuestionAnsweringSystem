9054154048b0764e7d7149914c75dbf51ac81c3d
jiezhou <zhoujie338@126.com>
2017-03-30 22:18:00 +0800

    Refactor the code.

--- deep-qa-web/src/main/java/org/apdplat/qa/api/AskServlet.java
+++ deep-qa-web/src/main/java/org/apdplat/qa/api/AskServlet.java
@@ -1,40 +1,39 @@
 /**
- * 
  * APDPlat - Application Product Development Platform
  * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
- * 
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
  */
 
 package org.apdplat.qa.api;
 
 import edu.stanford.nlp.util.StringUtils;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apdplat.qa.SharedQuestionAnsweringSystem;
 import org.apdplat.qa.model.CandidateAnswer;
 import org.apdplat.qa.model.Question;
 import org.apdplat.qa.util.ChineseCharUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * Json over HTTP 接口
@@ -55,22 +54,29 @@ public class AskServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("application/json;charset=UTF-8"); 
-        response.setCharacterEncoding("UTF-8"); 
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
         request.setCharacterEncoding("UTF-8");
-        
+
         String questionStr = request.getParameter("q");
         if (ChineseCharUtils.isChinese(questionStr)) {
             questionStr = ChineseCharUtils.trimAllBlankChar(questionStr);
         }
 
         String n = request.getParameter("n");
-        if (org.apache.commons.lang.StringUtils.isBlank(n)) {
+
+        if (org.apache.commons.lang.StringUtils.isBlank(questionStr)) {
             LOG.info("the input question is：" + questionStr);
             throw new ServletException("The question could not be blank");
         }
+
+        if (isIllegalTopN(n)) {
+            LOG.info("the input n is：" + n);
+            throw new ServletException("The parameter n is illegal, please check!");
+        }
+
         int topN = -1;
-        if(n != null && StringUtils.isNumeric(n)){
+        if (n != null && StringUtils.isNumeric(n)) {
             topN = Integer.parseInt(n);
         }
         Question question = null;
@@ -85,11 +91,16 @@ public class AskServlet extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             String json = JsonGenerator.generate(candidateAnswers, topN);
             out.println(json);
-            LOG.info("答案："+json);
+            LOG.info("答案：" + json);
         }
     }
 
+    private boolean isIllegalTopN(String n) {
+        return org.apache.commons.lang.StringUtils.isBlank(n) || !StringUtils.isNumeric(n);
+    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
