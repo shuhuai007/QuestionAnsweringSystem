e072351b617478a08d5b435dab0d577515cb82e6
jiezhou <zhoujie338@126.com>
2017-04-05 18:18:58 +0800

    Refactor the code.

--- deep-qa/src/main/java/org/apdplat/qa/system/QuestionAnsweringSystemImpl.java
+++ deep-qa/src/main/java/org/apdplat/qa/system/QuestionAnsweringSystemImpl.java
@@ -1,23 +1,3 @@
-/**
- * 
- * APDPlat - Application Product Development Platform
- * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
-
 package org.apdplat.qa.system;
 
 import java.util.ArrayList;
@@ -50,6 +30,7 @@ import org.apdplat.qa.select.CandidateAnswerSelect;
 import org.apdplat.qa.select.CommonCandidateAnswerSelect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /**
  * 使用此问答系统实现要指定4个组件：
