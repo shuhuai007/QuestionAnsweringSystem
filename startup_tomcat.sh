efdea582f1e26eeded8e486cc285ede725e57757
Jie Zhou <zhoujie338@126.com>
2017-04-21 15:38:26 +0800

    modify url name

--- startup_tomcat.sh
+++ startup_tomcat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export MAVEN_OPTS="-Xms3g -Xmx3g"
+WAR_VERSION=thougtworks-cieg-0.1
 
 if [ -f ~/tomcat-8.0.27.zip ] ; then
     echo tomcat-8.0.27 prepared
@@ -15,9 +16,9 @@ else
 fi
 mvn clean install
 echo clean install finished
-rm -rf ~/tomcat-8.0.27/apache-tomcat-8.0.27/webapps/deep-qa-web-1.2
+rm -rf ~/tomcat-8.0.27/apache-tomcat-8.0.27/webapps/${WAR_VERSION}
 echo old webapps directory deleted
-cp deep-qa-web/target/deep-qa-web-1.2.war ~/tomcat-8.0.27/apache-tomcat-8.0.27/webapps/
+cp deep-qa-web/target/${WAR_VERSION}.war ~/tomcat-8.0.27/apache-tomcat-8.0.27/webapps/
 echo copy war finished
 rm -rf ~/tomcat-8.0.27/apache-tomcat-8.0.27/logs
 echo old logs directory deleted
@@ -25,6 +26,6 @@ mkdir ~/tomcat-8.0.27/apache-tomcat-8.0.27/logs
 echo mkdir logs directory
 chmod +x ~/tomcat-8.0.27/apache-tomcat-8.0.27/bin/*.sh
 ~/tomcat-8.0.27/apache-tomcat-8.0.27/bin/catalina.sh start
-echo deep-qa-web-1.2 has been started
+echo ${WAR_VERSION} has been started
 
 tail -f ~/tomcat-8.0.27/apache-tomcat-8.0.27/logs/*
